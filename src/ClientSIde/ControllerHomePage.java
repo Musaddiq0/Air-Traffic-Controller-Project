343d97be45fbc1899e67c7a32de0ebe66947a407
Musaddiq <Sadiqmuhammad253@gmail.com>
2023-07-03 15:34:24 +0100

    app UI settings

--- src/ClientSIde/ControllerHomePage.java
+++ src/ClientSIde/ControllerHomePage.java
@@ -1,4 +1,15 @@
 package ClientSIde;
 
+import javax.swing.*;
+
 public class ControllerHomePage {
+    private JButton connectButton;
+    private JLabel homepageStatusLabel;
+    private JPanel mainPanel;
+    private JTabbedPane MaintabbedPane1;
+    private JPanel AirPortsPanel;
+    private JPanel Active;
+    private JTabbedPane airportOptionTab;
+    private JPanel routesMainTab;
+    private JTabbedPane routesOptionTab;
 }
